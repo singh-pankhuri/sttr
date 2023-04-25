921e896ee771a5c8294ae4785663f92de490722d
pankhuri-s30 <pankhurisingh@Pankhuris-MacBook-Air-2.local>
2023-04-25 11:42:12 -0400

    update to latest code

--- train.py
+++ train.py
@@ -5,33 +5,28 @@ from torch import optim
 import torch.utils.data as data
 from tqdm import tqdm
 from models import *
+import numpy as np
 
-def calculate_acc(prob, label, f2):
-  acc_train = [0, 0, 0, 0]
-  recall_train = [0, 0, 0, 0]
-  ndcg_train = [0, 0, 0, 0]
+def calculate_acc(prob, label, f2): 
+    recall = [0.0, 0.0, 0.0, 0.0]
+    ndcg = [0.0, 0.0, 0.0, 0.0]
+    for i, k in enumerate([1, 5, 10, 20]):
+        _, topk_predict_batch = torch.topk(prob, k=k)
+        for j, topk_predict in enumerate(to_npy(topk_predict_batch)):
+          target = to_npy(label)[j]
+          if target in topk_predict:
+            recall[i] = 1.0
+            rank_list = list(topk_predict)
+            rank_index = rank_list.index(target)
+            dcg = 5.0 / np.log2(rank_index + 2)
+            idcg = 5.0 / np.log2(2)
+            ndcg[i] = dcg / idcg
+          # if k == 20:
+          #   f2.write('next loc:{}'.format(label[0]))
+          #   f2.write('rec loc:{}'.format(topk_predict_batch))
+          #   f2.write('\n')
 
-  for i, k in enumerate([1, 5, 10, 20]):
-      # topk_batch (N, k)
-      _, topk_predict_batch = torch.topk(prob, k=k)
-      for j, topk_predict in enumerate(to_npy(topk_predict_batch)):
-          # topk_predict (k)
-          if to_npy(label)[j] in topk_predict:
-              acc_train[i] += 1
-              recall_train[i] += 1
-
-          # ndcg
-          idcg = np.sum(1 / np.log2(np.arange(2, k + 2)))
-          dcg = np.sum([1 / np.log2(rank + 2) if loc in topk_predict else 0 for rank, loc in enumerate(to_npy(topk_predict_batch)[j])])
-          ndcg_train[i] += dcg / idcg
-
-          if k == 20:
-              f2.write('next loc:{}'.format(label[j]))
-              f2.write('rec loc:{}'.format(topk_predict_batch))
-              f2.write('\n')
-
-  return np.array(acc_train), np.array(recall_train), np.array(ndcg_train)
-
+    return np.array(recall), np.array(ndcg) 
 
 def sampling_prob(prob, label, num_neg):
     num_label, l_m = prob.shape[0], prob.shape[1]-1  # prob (N, L)
@@ -58,13 +53,13 @@ def sampling_prob(prob, label, num_neg):
     return torch.FloatTensor(init_prob), torch.LongTensor(init_label)  # (N, num_neg+num_label), (N)
 
 
-class DataSet(data.Dataset):#数据集
+class DataSet(data.Dataset):
     def __init__(self, traj, m1, v, label, length):
         # (NUM, M, 3), (NUM, M, M, 2), (L, L), (NUM, M), (NUM), (NUM)
         self.traj, self.mat1, self.vec, self.label, self.length = traj, m1, v, label, length
 
     def __getitem__(self, index):
-        traj = self.traj[index].to(device)#拷贝到GPU
+        traj = self.traj[index].to(device)
         mats1 = self.mat1[index].to(device)
         vector = self.vec[index].to(device)
         label = self.label[index].to(device)
@@ -74,18 +69,17 @@ class DataSet(data.Dataset):#数据集
     def __len__(self):  # no use
         return len(self.traj)
 
-
 class Trainer:
-    def __init__(self, model, record, load, trajs, mat1, mat2t, labels, lens, mat2s):
+    def __init__(self, model, record, load, trajs, mat1, mat2t, labels, lens, mat2s, num_neg, lr, epochs):
         # load other parameters
         self.model = model.to(device)
         self.records = record
         self.start_epoch = record['epoch'][-1] if load else 1
-        self.num_neg = 10
+        self.num_neg = num_neg #10
         self.interval = 1000
         self.batch_size = 1 # N = 1
-        self.learning_rate = 3e-3
-        self.num_epoch = 100
+        self.learning_rate = lr  #3e-3
+        self.num_epoch = epochs #100
         self.threshold = np.mean(record['acc_valid'][-1]) if load else 0  # 0 if not update
 
         # (NUM, M, 3), (NUM, M, M, 2), (L, L), (NUM, M, M), (NUM, M), (NUM) i.e. [*M]
@@ -95,31 +89,29 @@ class Trainer:
         self.dataset = DataSet(self.traj, self.mat1, self.mat2t, self.label-1, self.len)
         self.data_loader = data.DataLoader(dataset=self.dataset, batch_size=self.batch_size, shuffle=False)
 
-    def train(self, part, start, dname):
-        # set optimizer 优化器
+    def train(self, part, start, dname, emb_dim):
+        # set optimizer
         optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=0)
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=1)
-        f=open((dname + "_result.txt"),"w",encoding='utf-8')
-        f2 = open("rec_point.txt", "w", encoding='utf-8')
+
+        f = open((dname + "_" + str(part) + "_" + str(self.num_neg) + "_" + str(emb_dim) + "_result.txt"),"w",encoding='utf-8')
+        # f2 = open((dname + "_" + str(part) + "_" + str(self.num_neg) + "_" + str(emb_dim) +"_rec_point.txt"), "w", encoding='utf-8')
+        f2 = open((dname +"_rec_point.txt"), "w", encoding='utf-8')
+
+
         for t in range(self.num_epoch):
             # settings or validation and test
             valid_size, test_size = 0, 0
-            acc_valid, acc_test = [0, 0, 0, 0], [0, 0, 0, 0]
-            recall_valid, recall_test = [0, 0, 0, 0], [0, 0, 0, 0]
-            ndcg_valid, ndcg_test = [0, 0, 0, 0], [0, 0, 0, 0]
+            recall_valid, recall_test = [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]
+            ndcg_valid, ndcg_test = [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]
 
             bar = tqdm(total=part)
             for step, item in enumerate(self.data_loader):
                 # get batch data, (N, M, 3), (N, M, M, 2), (N, M, M), (N, M), (N)
                 person_input, person_m1, person_m2t, person_label, person_traj_len = item
-
-                # first, try batch_size = 1 and mini_batch = 1
-
                 input_mask = torch.zeros((self.batch_size, max_len, 3), dtype=torch.long).to(device)
                 m1_mask = torch.zeros((self.batch_size, max_len, max_len, 2), dtype=torch.float32).to(device)
                 for mask_len in range(1, person_traj_len[0]+1):  # from 1 -> len
-                    # if mask_len != person_traj_len[0]:
-                    #     continue
                     input_mask[:, :mask_len] = 1.
                     m1_mask[:, :mask_len, :mask_len] = 1.
 
@@ -127,6 +119,7 @@ class Trainer:
                     train_m1 = person_m1 * m1_mask
                     train_m2t = person_m2t[:, mask_len - 1]
                     train_label = person_label[:, mask_len - 1]  # (N)
+                    # print("shape", train_label.shape)
                     train_len = torch.zeros(size=(self.batch_size,), dtype=torch.long).to(device) + mask_len
 
                     prob = self.model(train_input, train_m1, self.mat2s, train_m2t, train_len)  # (N, L)!!!
@@ -142,24 +135,20 @@ class Trainer:
 
                     elif mask_len == person_traj_len[0] - 1:  # only validation
                         valid_size += person_input.shape[0]
-                        # v_prob_sample, v_label_sample = sampling_prob(prob_valid, valid_label, self.num_neg)
-                        # loss_valid += F.cross_entropy(v_prob_sample, v_label_sample, reduction='sum')
-                        # acc_valid += calculate_acc(prob, train_label, f2)
-                        recall_valid += calculate_acc(prob, train_label, f2)[1]
-                        ndcg_valid = calculate_acc(prob, train_label, f2)[2]
+                        recall_valid += calculate_acc(prob, train_label, f2)[0]
+                        ndcg_valid += calculate_acc(prob, train_label, f2)[1]
 
                     elif mask_len == person_traj_len[0]:  # only test
                         test_size += person_input.shape[0]
-                        # v_prob_sample, v_label_sample = sampling_prob(prob_valid, valid_label, self.num_neg)
-                        # loss_valid += F.cross_entropy(v_prob_sample, v_label_sample, reduction='sum')
-                        # acc_test += calculate_acc(prob, train_label, f2)
-                        recall_test += calculate_acc(prob, train_label, f2)[1]
-                        ndcg_test = calculate_acc(prob, train_label, f2)[2]
+                        recall_test += calculate_acc(prob, train_label, f2)[0]
+                        ndcg_test += calculate_acc(prob, train_label, f2)[1]
 
                 bar.update(self.batch_size)
             bar.close()
 
+            np.set_printoptions(precision=6)
             recall_valid = np.array(recall_valid) / valid_size
+            ndcg_valid = np.array(ndcg_valid) / valid_size
             print('epoch:{}, time:{}, valid_recall:{}'.format(self.start_epoch + t, time.time() - start, recall_valid))
             # with open("result.txt","w") as f:
             f.write('epoch:{}, time:{}, valid_recall:{}'.format(self.start_epoch + t, time.time() - start, recall_valid))
@@ -169,12 +158,13 @@ class Trainer:
             f.write('\n')
 
             recall_test = np.array(recall_test) / test_size
+            ndcg_test = np.array(ndcg_test) / test_size
             print('epoch:{}, time:{}, test_recall:{}'.format(self.start_epoch + t, time.time() - start, recall_test))
             # with open("result.txt", "w") as f:
             f.write('epoch:{}, time:{}, test_recall:{}'.format(self.start_epoch + t, time.time() - start, recall_test))
             f.write('\n')
-            print('epoch:{}, time:{}, valid_ndcg:{}'.format(self.start_epoch + t, time.time() - start, ndcg_test))
-            f.write('epoch:{}, time:{}, valid_ndcg:{}'.format(self.start_epoch + t, time.time() - start, ndcg_test))
+            print('epoch:{}, time:{}, test_ndcg:{}'.format(self.start_epoch + t, time.time() - start, ndcg_test))
+            f.write('epoch:{}, time:{}, test_ndcg:{}'.format(self.start_epoch + t, time.time() - start, ndcg_test))
             f.write('\n')
 
             self.records['recall_valid'].append(recall_valid)
@@ -183,11 +173,10 @@ class Trainer:
             self.records['ndcg_test'].append(ndcg_test)
             self.records['epoch'].append(self.start_epoch + t)
 
-            if self.threshold < np.mean(acc_valid):
-                self.threshold = np.mean(acc_valid)
-                # save the model
-                torch.save({'state_dict': self.model.state_dict(),
-                            'records': self.records,
-                            'time': time.time() - start},
-                           'best_stan_win_1000_' + dname + '.pth')
+            # if self.threshold < np.mean(recall_valid):
+            #     self.threshold = np.mean(recall_valid)
+            #     torch.save({'state_dict': self.model.state_dict(),
+            #                 'records': self.records,
+            #                 'time': time.time() - start},
+            #                'best_stan_win_1000_' + dname + '.pth')
         f.close()
